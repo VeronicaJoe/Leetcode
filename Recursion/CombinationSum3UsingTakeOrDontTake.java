da796d8a1e897d937cb3d582d087d763c3da8df8
VeronicaJoe <118820715+VeronicaJoe@users.noreply.github.com>
2026-07-25 20:28:44 +0530

    Refactor CombinationSum3 to use ArrayList instead of HashSet

--- Recursion/CombinationSum3UsingTakeOrDontTake.java
+++ Recursion/CombinationSum3UsingTakeOrDontTake.java
@@ -2,18 +2,18 @@ class Solution {
     //Brute Force: Using Take / Dont Take
     public List<List<Integer>> combinationSum3(int k, int n) 
     {
-        Set<List<Integer>> res = new HashSet<>();
+        List<List<Integer>> res = new ArrayList<>();
         int[] arr = {1,2,3,4,5,6,7,8,9};
         generate(0,n,arr,k,new ArrayList<>(),res);
-        return new ArrayList<>(res);    
+        return res;    
     }
-    private void generate(int ind,int target,int[] arr,int len,List<Integer> curr,Set<List<Integer>> set)
+    private void generate(int ind,int target,int[] arr,int len,List<Integer> curr,List<List<Integer>> res)
     {
         if(ind>=arr.length)
         {
             if(target==0&&curr.size()==len)
             {
-                set.add(new ArrayList<>(curr));
+                res.add(new ArrayList<>(curr));
             }
             return;
         }
@@ -22,9 +22,9 @@ class Solution {
         if(arr[ind]<=target)
         {
             curr.add(arr[ind]);
-            generate(ind+1,target-arr[ind],arr,len,curr,set);
+            generate(ind+1,target-arr[ind],arr,len,curr,res);
             curr.remove(curr.size()-1);
         }
-        generate(ind+1,target,arr,len,curr,set);
+        generate(ind+1,target,arr,len,curr,res);
     }
 }
