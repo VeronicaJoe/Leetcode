20a3c766a64fbe9c484cd37d22548869bf1ca168
VeronicaJoe <118820715+VeronicaJoe@users.noreply.github.com>
2026-09-07 16:59:53 +0530

    Refactor Prim's algorithm for clarity and consistency
    Refactor Prim's algorithm implementation for clarity and consistency. Updated variable names and comments for better understanding.

--- Graph/MST/PrimsAlgorithm.java
+++ Graph/MST/PrimsAlgorithm.java
@@ -1,8 +1,7 @@
 class Solution {
-    public int spanningTree(int V, int[][] edges) 
-    {
-        //PRIM'S ALGORITHM - Lets us know the min path sum in a MST and the adjacency list of that MST
-        //Step 1: Construct the adj list
+    public int spanningTree(int V, int[][] edges) {
+        // code here
+        //Step 1: Get the adjacency list
         List<List<int[]>> adj = new ArrayList<>();
         for(int i=0;i<V;i++)
         {
@@ -14,44 +13,54 @@ class Solution {
             int v = edge[1];
             int wt = edge[2];
             
+            //bidirectional
             adj.get(u).add(new int[]{v,wt});
             adj.get(v).add(new int[]{u,wt});
         }
-        //Step 2: Initial Configurations:
+        
+        //Step 2: Initial Config:
+        //        Visited[], PQ<wt,node,parent>, sum = 0, mst edges list<parent,node>
         boolean[] visited = new boolean[V];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(a[0],b[0]));//<wt,node,parent>
-        List<int[]> mst = new ArrayList<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(a[0],b[0]));//minheap
+        List<int[]> mstList = new ArrayList<>();
         int sum = 0;
         
-        pq.offer(new int[]{0,0,-1});
+        //Step 3: intially add srcNode = 0 to PQ
+        pq.offer(new int[]{0,0,-1});//no parent and no edge wt ad src = 0
+        
+        //Step 4: Till the PQ is not empty
+        //      i)Pop from PQ
+        //      ii)Check if node already visited - skip
+        //      iii)Mark node as visited
+        //      iv)If parent is -1 then dont add to MSTList and sum
+        //      v)Else add to MSTList and sum up
+        //      vi) Get the adj nodes of node if they are not visited add to PQ
+        
         while(!pq.isEmpty())
         {
             int[] element = pq.poll();
             int wt = element[0];
-            int v = element[1];//node
-            int u = element[2];//parent
+            int node = element[1];
+            int parent = element[2];
             
-            if(visited[v])
+            if(visited[node])
             continue;
             
-            
-            //(1) Mark as visited
-            visited[v]=true;
-            //(2) Add node to sum
+            visited[node] = true;
             sum+=wt;
-            //(3) Check if parent exist then add to MST adj list
-            if(u!=-1)
+            if(parent!=-1)
             {
-                mst.add(new int[]{u,v});//parent,node
+                mstList.add(new int[]{parent,node});
             }
-            //(4) Get the neighbors
-            for(int[] neighbor:adj.get(v))
+            //get neighbors
+            for(int[] neighbor:adj.get(node))
             {
                 int neighborVal = neighbor[0];
-                int edgeWt = neighbor[1];
-                
+                int neighborWt = neighbor[1];
                 if(!visited[neighborVal])
-                pq.offer(new int[]{edgeWt,neighborVal,v});//<wt,node,parent>
+                {
+                    pq.offer(new int[]{neighborWt,neighborVal,node});
+                }
             }
         }
         return sum;
